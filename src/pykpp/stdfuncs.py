b525672263e653cfe6bd5539800520d14b71cdee
Barron Henderson <barronh@gmail.com>
2013-05-21 22:43:17 -0400

    Added TEMP as func property

--- src/pykpp/stdfuncs.py
+++ src/pykpp/stdfuncs.py
@@ -1,29 +1,30 @@
 from numpy import *
+from scipy.constants import *
 
-def ARR( A0, B0, C0, TEMP ):
-    out = A0 * exp(-B0 / TEMP) * (TEMP / 300.0)**(C0)
+def ARR( A0, B0, C0 ):
+    out = A0 * exp(-B0 / ARR.TEMP) * (ARR.TEMP / 300.0)**(C0)
     return out
 
-def ARR2( A0, B0, TEMP):
-    out = A0 * exp( B0 / TEMP )
+def ARR2( A0, B0):
+    out = A0 * exp( B0 / ARR2.TEMP )
     return out
 
-def EP2(A0, C0, A2, C2, A3, C3, TEMP):
-    K0 = A0 * exp(-C0 / TEMP)
-    K2 = A2 * exp(-C2 / TEMP)
-    K3 = A3 * exp(-C3 / TEMP)
-    K3 = K3 * M * 1.0E6
+def EP2(A0, C0, A2, C2, A3, C3):
+    K0 = A0 * exp(-C0 / EP2.TEMP)
+    K2 = A2 * exp(-C2 / EP2.TEMP)
+    K3 = A3 * exp(-C3 / EP2.TEMP)
+    K3 = K3 * EP2.M * 1.0E6
     return K0 + K3 / (1.0 + K3 / K2 )
 
-def EP3(A1, C1, A2, C2, TEMP):
-    K1 = A1 * exp(-C1 / TEMP)
-    K2 = A2 * exp(-C2 / TEMP)
-    return K1 + K2 * (1.0E6 * M)
+def EP3(A1, C1, A2, C2):
+    K1 = A1 * exp(-C1 / EP3.TEMP)
+    K2 = A2 * exp(-C2 / EP3.TEMP)
+    return K1 + K2 * (1.0E6 * EP3.M)
 
-def FALL ( A0, B0, C0, A1, B1, C1, CF, TEMP, M):
+def FALL ( A0, B0, C0, A1, B1, C1, CF):
     K0 = ARR(A0, B0, C0)
     K1 = ARR(A1, B1, C1)
-    K0 = K0 * M * 1.0E6
+    K0 = K0 * FALL.M * 1.0E6
     K1 = K0 / K1
     return (K0 / (1.0 + K1))* CF**(1.0 / (1.0 + (log10(K1))**2))
 
@@ -37,3 +38,13 @@ def k_3rd(temp, cair, k0_300K, n, kinf_300K, m, fc):
 
 def k_arr (k_298, tdep, temp):
     return k_298 * exp(tdep * (1. / temp - 3.3540E-3)) # 1/298.15=3.3540e-3
+
+def update_func_world(world):
+    try:
+        FALL.M = world['M']
+    except:
+        try:
+            FALL.M = eval('P / (R / centi**3) / TEMP * Avogadro', None, world)
+        except:
+            pass
+    ARR2.TEMP = ARR.TEMP = EP2.TEMP = EP3.TEMP = world['TEMP']
