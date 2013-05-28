e4ad6b8ea441def97287f3001ead0f330b6ef674
Barron Henderson <barronh@gmail.com>
2013-05-28 11:35:37 -0400

    Add default output and output path control

--- src/pykpp/__main__.py
+++ src/pykpp/__main__.py
@@ -9,6 +9,8 @@ values and reaction definitions according to the KPP
 format.
 """)
 
+parser.add_option("-v", "--verbose", dest = "verbose", action = "store_true", default = False, help = "Show extended output")
+
 parser.add_option("-a", "--atol", dest = "atol", type = "float", default = 1e-3, help = "absolute tolerance")
 
 parser.add_option("-r", "--rtol", dest = "rtol", type = "float", default = 1e-4, help = "relative tolerance")
@@ -17,11 +19,15 @@ parser.add_option("", "--tstart", dest = "tstart", type = "float", default = Non
 
 parser.add_option("", "--tend", dest = "tend", type = "float", default = None, help = "End time")
 
+parser.add_option("", "--norun", dest = "norun", action = 'store_true', default = False, help = "Don't run")
+
 parser.add_option("", "--dt", dest = "dt", type = "float", default = None, help = "Time step")
 
 parser.add_option("-j", "--jacobian", dest = "jacobian", action="store_true", default = False, help = "Enable use of jacobian")
 
 parser.add_option("-k", "--keywords", dest = "keywords", type="string", default = 'hv', help = "List of keywords to be ignored in reactants or products (comma delimited; default='hv')")
+
+parser.add_option("-o", "--outpath", dest = "outpath", type="string", default = None, help = "Output path.")
 
 parser.add_option("-s", "--solver", dest = "solver", default = 'lsoda', help = "solver (default: lsoda; vode; zvode; dopri5; dop853)")
 
@@ -32,9 +38,8 @@ if len(args) == 0:
     exit()
     
 for arg in args:
-    out = Mech(arg, verbose = True, keywords = [k_.strip() for k_ in options.keywords.split(',')])
-    runtime = out.run(tstart = options.tstart, tend = options.tend, dt = options.dt, solver = options.solver, jac = options.jacobian, atol = options.atol, rtol = options.rtol)
-    print 'Solved in %f seconds' % runtime
-    out.print_world(format = '%.4e', verbose = True)
-    #for rxni, (rxn, rate) in enumerate(zip(out.get_rxn_strs(), out.world['rate_const'])):
-    #    print '%d: %.2e,' % (rxni + 1, rate)
+    out = Mech(arg, verbose = options.verbose, keywords = [k_.strip() for k_ in options.keywords.split(',')])
+    if not options.norun:
+        runtime = out.run(tstart = options.tstart, tend = options.tend, dt = options.dt, solver = options.solver, jac = options.jacobian, atol = options.atol, rtol = options.rtol)
+        print 'Solved in %f seconds' % runtime
+        out.output(options.outpath)
