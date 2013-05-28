6b5a6cf87c6a173f50abb11ad96f6bc226e40414
Barron Henderson <barronh@gmail.com>
2013-05-28 11:37:24 -0400

    Fully functional KPP interpretation with certain sections ignored.

--- src/pykpp/parse.py
+++ src/pykpp/parse.py
@@ -2,23 +2,31 @@ __all__ = '_parsefile _reactionstoic _stoicreaction _allspc'.split()
 from pyparsing import *
 import os
 import re
+from warnings import warn
+
+RegexM = lambda expr: Regex(expr, flags = re.MULTILINE)
+RegexMA = lambda expr: Regex(expr, flags = re.MULTILINE + re.DOTALL)
 
 spcname = Word(alphas, bodyChars = alphanums + '_')
-real = Combine(Optional(Optional(oneOf("+ -")) + Word(nums)) +
-               Optional("." + Word(nums)) +
-               Optional(oneOf("e E") + Optional(oneOf("+ -")) +
-               Word(nums))
-              ).setName("real").setParseAction( lambda toks: '1.' if toks[0] == '' else toks[0] )
 
-stoic = Group(Optional(real, default = 1.) + spcname).setResultsName('stoic')
+real = Combine(Optional(oneOf("+ -")) + Word(nums) +
+               Optional("." + Optional(Word(nums)) +
+               Optional(oneOf("e E d D") + Optional(oneOf("+ -")) +
+               Word(nums)))
+              ).setName("real").setParseAction( lambda toks: '1.' if toks[0] == '' else toks[0].replace('D', 'E'))
+              
+#stoic = Group(Optional(real, default = '1.') + spcname).setResultsName('stoic')
 
-inlinecomment = Suppress('{' + Word(alphanums + '*+-/%.;)([] ').setResultsName('rate') + '}')
+stoic = Group(Optional(Combine(Optional(oneOf('+ -') + White(), default = '') + Optional(real, default = '1.'))) + spcname).setResultsName('stoic')
+
+inlinecomment = Suppress('{' + Word(alphanums + '*+-/%.;)([] ').setResultsName('inline') + '}')
 lbl = Optional(Suppress('<') + Suppress(ZeroOrMore(' ')) + Regex('[^>]+').setResultsName("label") + Suppress('>'))
-rcts = Group(delimitedList(Optional(inlinecomment) + stoic + Optional(inlinecomment), '+')).setResultsName("reactants")
-prods = Group(delimitedList(Optional(inlinecomment) + stoic + Optional(inlinecomment), Regex('[+]'))).setResultsName("products")
-rate = Regex('[^;]+').setResultsName('rate')
+rcts = Group(delimitedList(Optional(inlinecomment) +stoic + Optional(inlinecomment), '+')).setResultsName("reactants")
+rcts = Group(OneOrMore(Optional(inlinecomment) +stoic + Optional(inlinecomment))).setResultsName("reactants")
+prods = Group(OneOrMore(Optional(inlinecomment) + stoic + Optional(inlinecomment))).setResultsName("products")
+rate = RegexM('[^;]+').setResultsName('rate')
 
-linecomment = Suppress('//' + Regex('.*?$'))
+linecomment = Suppress('//' + RegexM('.*?$'))
 reaction = Group(Suppress(Optional(White())) + lbl + rcts + Suppress('=') + prods + Suppress(':') + rate + Suppress(';' + Optional(White()) + Optional(inlinecomment) + Optional(White())))
 
 def reactions_parse(s, loc, toks):
@@ -28,44 +36,54 @@ def reactions_parse(s, loc, toks):
         raise ParseFatalException('Error in parsing EQUATIONS (reactions start on character %d; lines numbered within): ' % loc + str(e))
     return out
 
-reactions = Group(Suppress(lineStart + '#EQUATIONS') + Regex('[^#]+', flags = 16 + 8) + FollowedBy(Or([LineStart() + '#', stringEnd]))).setResultsName('EQUATIONS').addParseAction(reactions_parse)
+reactions = Group(Suppress(RegexM('^#EQUATIONS')) + RegexMA('.+?(?=^#)')).setResultsName('EQUATIONS').addParseAction(reactions_parse)
 
 #reactions = Group(Suppress(lineStart + '#EQUATIONS') + ZeroOrMore(inlinecomment) + OneOrMore(reaction) + ZeroOrMore(inlinecomment) + Optional(FollowedBy(lineStart + '#'))).setResultsName('reactions')
 
 
-assignment = Group(Optional(inlinecomment) + spcname + Optional(' ') + Suppress('=') + Regex('[^;]+', flags = 8) + Suppress(';') + Optional(inlinecomment))
+assignment = Group(Optional(inlinecomment) + spcname + Optional(' ') + Suppress('=') + RegexM('[^;]+') + Suppress(';') + Optional(inlinecomment))
 
 def initvalues_parse(loc, toks):
     try:
-        return OneOrMore(Or([inlinecomment, assignment])).parseString(toks[0][0], parseAll = True)
+        return [linecomment.addParseAction(lambda : '').transformString(inlinecomment.addParseAction(lambda : '').transformString(real.transformString('\n'.join([l_.strip() for l_ in toks[0][0].split('\n')]))))]
     except Exception, e:
         raise ParseFatalException('Error in parsing INITVALUES (reactions start on character %d; lines numbered within): ' % loc + str(e))
 
-initvalues = Group(Suppress(lineStart + '#INITVALUES') + Regex('[^#]+', flags = 16 + 8) + FollowedBy(Or([lineStart + '#', stringEnd]))).setResultsName('INITVALUES' )#.addParseAction(initvalues_parse)
+initvalues = Group(Suppress(RegexM('^#INITVALUES')) + RegexMA('.+?(?=^#)')).setResultsName('INITVALUES' ).addParseAction(initvalues_parse)
 
-worldupdater = Optional(Group(Suppress(lineStart + '#WORLDUPDATER') + Regex('[^#]*') + Optional(FollowedBy(lineStart + '#')))).setResultsName('WORLDUPDATER')
+worldupdater = Optional(Group(Suppress(lineStart + '#WORLDUPDATER') + Regex('[^#]+') + Optional(FollowedBy(lineStart + '#')))).setResultsName('WORLDUPDATER')
 
 def code_func(loc, toks):
     if toks[0][0] == 'F90_INIT':
-        return ParseResults(toks[0][1:], name = 'INIT')
+        return ParseResults([linecomment.addParseAction(lambda : '').transformString(inlinecomment.addParseAction(lambda : '').transformString(real.transformString('\n'.join([l_.strip() for l_ in toks[0][1].split('\n')]))))], name = 'INIT')
 
-codeseg = Group(Suppress(lineStart + "#INLINE ") + Regex('(F90|F77|C|MATLAB)_(INIT|GLOBAL|RCONST|RATES|UTIL)') + Regex('[^#]+', flags = 16 + 8) + Suppress('#ENDINLINE')).setResultsName('CODESEG').addParseAction(code_func)
+codeseg = Group(Suppress(RegexM("^#INLINE ")) + Regex('(F90|F77|C|MATLAB)_(INIT|GLOBAL|RCONST|RATES|UTIL)') + RegexMA('[^#]+') + Suppress('#ENDINLINE')).setResultsName('CODESEG').addParseAction(code_func)
 
-lookat = Optional(Group(Suppress(lineStart + '#LOOKAT') + Regex('.+') + lineEnd).setResultsName('LOOKAT'))
-monitor = Optional(Group(Suppress(lineStart + '#MONITOR') + Regex('.+') + lineEnd).setResultsName('MONITOR'))
-check = Optional(Group(Suppress(lineStart + '#CHECK') + Regex('.+') + lineEnd).setResultsName('CHECK'))
+monitor = Optional(Group(Suppress(RegexM('^#MONITOR')) + RegexM('.+')).setResultsName('MONITOR'))
 
-atoms = Optional(Group(Suppress(lineStart + '#ATOMS') + Regex('[^#]+', flags = re.MULTILINE + re.DOTALL) + FollowedBy(Or([lineStart + '#', stringEnd]))).setResultsName('ATOMS'))
+def ignoring(toks):
+    print 'Ignoring', toks[0][0]
+lookat = Optional(Group(Suppress(RegexM('^#LOOKAT')) + RegexM('.+')).setResultsName('LOOKAT'))
 
-defvar = Optional(Group(Suppress(lineStart + '#DEFVAR') + Regex('[^#]+', flags = re.MULTILINE) + FollowedBy(Or([lineStart + '#', stringEnd]))).setResultsName('DEFVAR'))
+check = Optional(Group(RegexM('^#CHECK') + RegexM('.+')).setResultsName('CHECK').addParseAction(ignoring))
 
-deffix = Optional(Group(Suppress(lineStart + '#DEFFIX') + Regex('[^#]+', flags = re.MULTILINE) + FollowedBy(Or([lineStart + '#', stringEnd]))).setResultsName('DEFFIX'))
+atoms = Optional(Group(RegexM('^#ATOMS') + RegexMA('.+?(?=^#)')).setResultsName('ATOMS').addParseAction(ignoring))
 
-language = Optional(Suppress(Regex('^#LANGUAGE.+$')))
-integrator = Optional(Regex('^#INTEGRATOR.+$'), default = 'lsoda').setResultsName('INTEGRATOR')
-driver = Optional(Suppress(Regex('^#DRIVER.+$')))
+defvar = Optional(Group(RegexM('^#DEFVAR') + RegexMA('.+?(?=^#)')).setResultsName('DEFVAR').addParseAction(ignoring))
 
-parser = Each([initvalues, atoms, deffix, defvar, reactions, lookat, monitor, check, integrator, driver, OneOrMore(codeseg), ZeroOrMore(linecomment)])
+deffix = Optional(Group(RegexM('^#DEFFIX') + RegexMA('.+?(?=^#)')).setResultsName('DEFFIX').addParseAction(ignoring))
+
+integrator = Optional(Group(RegexM('^#INTEGRATOR') + RegexM('.+')), default = 'lsoda').setResultsName('INTEGRATOR')
+
+language = Optional(Group(RegexM('^#LANGUAGE') + RegexM('.+')).setResultsName('LANGUAGE').addParseAction(ignoring))
+
+driver = Optional(Group(RegexM('^#DRIVER') + RegexM('.+')).addParseAction(ignoring))
+
+elements = [language, Optional(initvalues), atoms, deffix, defvar, reactions, lookat, monitor, check, integrator, driver, ZeroOrMore(codeseg), ZeroOrMore(linecomment)]
+
+for i in elements:
+    i.verbose_stacktrace = True
+parser = Each(elements)
 
 includepaths = ['.']
 def includeit(matchobj):
@@ -81,7 +99,7 @@ def includeit(matchobj):
 def _parsefile(path):
     reinclude = re.compile('^#include (.+)', re.M)
     lcomment = re.compile('^//.*', re.M)
-    includepaths.append(os.path.dirname(path))
+    includepaths.insert(0, os.path.dirname(path))
     old = ''
     deftext = file(path).read()
     while old != deftext:
@@ -89,13 +107,11 @@ def _parsefile(path):
         deftext = reinclude.sub(includeit, deftext)
     
     deftext = lcomment.sub('', deftext)
+    #file('test.txt', 'w').write(deftext)
     return parser.parseString(deftext)
 
 def _allspc(parsed):
     spc = set()
-    #for k, v in parsed['initvalues']:
-    #    spc.add(k)
-    
     for rct in parsed['EQUATIONS']:
         for v, k in rct['reactants']:
             spc.add(k)
