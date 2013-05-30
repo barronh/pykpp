b2e0cbb9fa9f33724f0f2682bd4c794837f6a194
Barron Henderson <barronh@gmail.com>
2013-05-30 09:39:38 -0400

    Removing test.py as it has been superceded

--- src/pykpp/test.py
+++
@@ -1,5 +0,0 @@
-import scipy.integrate as itg
-from mech import Mech
-out = Mech('small_strato.eqn')
-out.run(startt = 12*3600., endt = (12+24*3) * 3600., dt = 3600.)
-out.print_world(verbose = True, format = '%.4e')
