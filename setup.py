88c032829efa3f8654ba4a2abe6015700a783052
barronh <barronh@gmail.com>
2013-06-14 13:03:45 -0300

    Adding submodule install commands

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(name = 'pykpp',
       url='https://github.com/barronh/pykpp/',
       download_url='https://github.com/barronh/pykpp/archive/master.zip',
       long_description="pykpp is a KPP-like chemical mechanism parser that produces a box model solvable by SciPy's odeint solver",
-      packages = ['pykpp'],
+      packages = ['pykpp', 'pykpp.tuv', 'pykpp.morpho', 'pykpp.models', 'pykpp.test'],
       package_dir = {'pykpp': 'src/pykpp'},
-      package_data = {'pykpp': ['*.eqn', '*.txt']},
+      package_data = {'pykpp.models': ['*.eqn', '*.txt']},
       requires = ['numpy', 'scipy', 'pyparsing']
       )
